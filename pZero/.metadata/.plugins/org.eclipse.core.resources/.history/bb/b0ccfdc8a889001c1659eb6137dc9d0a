470d9bd41880518f2eb749a897b9849e1579b2c8
JSotto09 <jacsotto09@gmail.com>
2022-02-09 08:19:13 -0500

    added some documentation to pZero

---
+++ pZero/.metadata/.plugins/org.eclipse.core.resources/.history/bb/b0ccfdc8a889001c1659eb6137dc9d0a
@@ -0,0 +1,16 @@
+package com.revature.p0.daos;
+
+import com.revature.p0.util.collections.List;
+
+public interface CrudDao<T> {
+	
+	T create(T newObj);
+	
+	List<T> findAll();
+	T findById(String id);
+	
+	boolean update(T updatedObj);
+	
+	boolean delete(String id);
+
+}
--- pZero/pZero/src/main/java/com/revature/p0/daos/CrudDao.java
+++ pZero/pZero/src/main/java/com/revature/p0/daos/CrudDao.java
@@ -2,15 +2,16 @@ package com.revature.p0.daos;
 
 import com.revature.p0.util.collections.List;
 
+//created interface dao
 public interface CrudDao<T> {
 	
-	T create(T newObj);
+	T create(T newObj); //creates a create method with one parameter that accepts any type
 	
-	List<T> findAll();
-	T findById(String id);
+	List<T> findAll(); //creates a list to find all of something
+	T findById(String id); // creates a find by idea constructor
 	
-	boolean update(T updatedObj);
+	boolean update(T updatedObj); //creates update
 	
-	boolean delete(String id);
+	boolean delete(String id); //creates delete
 
 }
